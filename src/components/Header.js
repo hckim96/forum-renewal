e874f7394f32c3429fe1209362ea0a9de971b9bc
hckim96 <hyeoncheolkim96@gmail.com>
2021-01-05 16:30:32 +0900

    WIP

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import "./Header.css";
 
 export const Header = () => {
     return (
-        <div style = {{display: "flex", alignItems: "center", textDecoration: "None", justifyContent: "space-between", height: "60px", boxShadow: "0px 0px 3px #000000"}}>
-            <Link to = "/">홈</Link>
-            <Link to = "/canvas">폭죽놀이</Link>
-            <Link to = "/post">포스트리스트</Link>
-            <Link to = "/write">글 쓰기</Link>
+        <div>
+            <div className = "header-container" style = {{display: "flex", alignItems: "center", textDecoration: "None", justifyContent: "space-between", height: "60px", boxShadow: "0px 0px 3px #000000"}}>
+                <NavLink to = "/" exact = {true} className="header-link" activeClassName = "header-activeLink"><span>홈</span></NavLink>
+                <NavLink to = "/canvas" className="header-link" activeClassName = "header-activeLink"><span>폭죽놀이</span></NavLink>
+                <NavLink to = "/post" className="header-link" activeClassName = "header-activeLink"><span>포스트리스트</span></NavLink>
+                <NavLink to = "/write" className="header-link" activeClassName = "header-activeLink"><span>글 쓰기</span></NavLink>
+            </div>
+            <div className = "header-underbar"></div>
+
         </div>
     )
 }
